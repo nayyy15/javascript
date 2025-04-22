9e0da3c998f781e8006ae2d79a221731bd61a2d1
nayyy15 <nayara.souzaribeiro15@gmail.com>
2025-04-22 11:27:22 -0300

    .

---
+++ LocalStorage/script.js
@@ -0,0 +1,17 @@
+// LOCALSTORAGE -> SCRIPT.JS
+
+// QUANDO CARREGAR A PÁGINA, MOSTRAR O QUE JÁ ESTAVA SALVO
+window.onload = function() {
+    const textoSalvo = localStorage.getItem("minhaAnotacao")
+    if (textoSalvo) {
+        document.getElementById("anotacao").value = textoSalvo
+    }
+}
+
+
+// SALVAR QUANDO CLICAR NO BOTÃO
+function salvarAnotacao() {
+    const conteudo = document.getElementById("anotacao").value;
+    localStorage.setItem("minhaAnotacao", conteudo)
+    alert("Nota salva com sucesso")
+}
